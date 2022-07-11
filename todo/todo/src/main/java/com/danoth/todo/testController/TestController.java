942c067620e97b464aec8920b2c8503384c2401c
simgyuhwan <rbghks19@naver.com>
2022-07-11 20:51:53 +0900

    feat: view 구현중

--- todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
+++ todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
@@ -1,16 +1,17 @@
 package com.danoth.todo.testController;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/test")
 public class TestController {
 
     @GetMapping
     public String Test(){
-        return "hello world man~~~~";
+        return "todo/todo";
     }
 
 
