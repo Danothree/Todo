3a36e729c6afaf374367a47a677ca96f703dd0bb
simgyuhwan <rbghks19@naver.com>
2022-07-11 10:33:55 +0900

    초기 설정

---
+++ todo/todo/src/test/java/com/danoth/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danoth.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
