613bd8225dbe9f27430bf22db3a065bd31d85a42
chu_veely@naver.com <mm910236>
2022-07-11 20:47:04 +0900

    feat: 레이아웃 커밋

--- todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
+++ todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
@@ -1,17 +1,22 @@
 package com.danoth.todo.testController;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/test")
+@Controller
+//@RequestMapping("/test")
 public class TestController {
 
-    @GetMapping
-    public String Test(){
-        return "hello world man~~~~";
-    }
+//    @GetMapping
+//    public String Test(){
+//        return "hello world man~~~~";
+//    }
 
+    @GetMapping("/list")
+    public String list() {
+        return "list";
+    }
 
 }
