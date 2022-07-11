8e63567d40443d3c7d4ecb3f05b4abf2da37281e
asd1029a <asd1029a>
2022-07-11 21:07:41 +0900

    teste

--- todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
+++ todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
@@ -1,16 +1,16 @@
 package com.danoth.todo.testController;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/test")
+@Controller
 public class TestController {
 
-    @GetMapping
+    @GetMapping("")
     public String Test(){
-        return "hello world man~~~~";
+        return "index";
     }
 
 
