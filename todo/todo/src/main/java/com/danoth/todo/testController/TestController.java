5b60385a10083ab8f9167a612c4014d12b38ebfc
EunJoo Kim <88094058+javajoo@users.noreply.github.com>
2022-07-12 09:37:40 +0900

    chore : 컴퓨터 깃 pull

--- todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
+++ todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
@@ -6,13 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-//@RequestMapping("/test")
 public class TestController {
-
-//    @GetMapping
-//    public String Test(){
-//        return "hello world man~~~~";
-//    }
 
     @GetMapping("/list")
     public String list() {
