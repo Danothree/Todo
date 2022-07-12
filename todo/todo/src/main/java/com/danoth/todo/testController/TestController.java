5e1a37e625dc768242d10829332300649d3e9c01
EunJoo Kim <88094058+javajoo@users.noreply.github.com>
2022-07-12 17:57:02 +0900

    feat : 추가 기능

--- todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
+++ todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
@@ -1,16 +1,36 @@
 package com.danoth.todo.testController;
 
+import com.danoth.todo.entitiy.Todolist;
+import com.danoth.todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class TestController {
 
-    @GetMapping("/list")
+    private final TodoService todoService;
+
+    @GetMapping("/todolist")
     public String list() {
-        return "list";
+        return "todolist";
+    }
+
+    @RequestMapping("/todolist")
+    public String list(Model model) {
+        List<Todolist> todolist = this.todoService.getList();
+        model.addAttribute("todolist",todolist);
+        return "todolist";
+    }
+
+    @PostMapping("/todolist/create")
+    public String todoCreate(@RequestParam String todolistContent) {
+        todoService.create(todolistContent);
+        return "redirect:/todolist"; // 주소경로에 공백도 인식하기 때문에 공백있으면 404 에러 뜬다.
     }
 
 }
