df0103c9ddb42c34e795b99f829b36667e8b9f7c
kjm <kimjm9347@danusys.com>
2022-07-12 17:56:34 +0900

    layout update

--- todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
+++ todo/todo/src/main/java/com/danoth/todo/testController/TestController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
-    @GetMapping("")
+    @GetMapping("/test")
     public String Test(){
-        return "index";
+        return "testest";
     }
 
 
