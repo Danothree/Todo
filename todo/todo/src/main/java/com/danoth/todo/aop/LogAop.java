3ab07ea2f5dbbfe434a6b923e0fedf71887701e3
simgyuhwan <rbghks19@naver.com>
2022-07-24 13:55:32 +0900

    feat: log aop 적용:

---
+++ todo/todo/src/main/java/com/danoth/todo/aop/LogAop.java
@@ -0,0 +1,56 @@
+package com.danoth.todo.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+
+@Slf4j
+@Aspect
+@Component
+public class LogAop {
+
+    @Pointcut("execution(* com.danoth.todo.controller..*.*(..))")
+    private void cut(){}
+
+    @Before("cut()")
+    public void beforeParameterLog(JoinPoint joinPoint){
+        // 메서드 정보
+        Method method = getMethod(joinPoint);
+        log.info("======== method name = {} =======", method.getName());
+
+        // 파라미터 정보
+        Object[] args = joinPoint.getArgs();
+        if(args.length <= 0){
+            log.info("no parameter");
+            return;
+        }
+        for (Object arg : args) {
+            log.info("parameter type = {} ", arg.getClass().getSimpleName());
+            log.info("parameter value = {} ", arg);
+        }
+    }
+
+    // PointCut 이후
+    @AfterReturning(value = "cut()", returning = "returnObj")
+    public void afterReturnLog(JoinPoint joinPoint, Object returnObj){
+        // Method 정보
+        Method method = getMethod(joinPoint);
+        log.info("========== method name == {} =======", method.getName());
+
+        log.info("return type = {} ", returnObj.getClass().getSimpleName());
+        log.info("return value = {} ", returnObj);
+    }
+
+    private Method getMethod(JoinPoint joinPoint){
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        return signature.getMethod();
+    }
+}
