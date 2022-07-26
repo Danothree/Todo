a15935949d0f076e55524a11035d086c7c1478c6
asd1029a <xxx1029x@gmail.com>
2022-07-26 19:45:44 +0900

    crud update

--- todo/todo/src/main/resources/static/js/page/listTable.js
+++ todo/todo/src/main/resources/static/js/page/listTable.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded',function(){
-    getList();
+
 });
 window.onload = function(){
+    getList();
     // 클릭 이벤트
     document.getElementById('addBtn').addEventListener('click',() => {
         const userId = document.getElementById('userId').value;
@@ -37,15 +38,19 @@ function getList() {
         if(xhr.readyState === 4) {
             console.log('통신 완료');
             if(xhr.status === 200) {
+                let tag = '';
                 console.log(xhr.response);
-                let response = xhr.response;
-                console.log(response.content);
+                let response = JSON.parse(xhr.response);
+                let inSpan = document.querySelector('.inSpan');
+                console.log(response[0].content);
+                console.log(response.length);
                 for (let i = 0; i < response.length; i++) {
-                    console.log(response[i].content);
-                    console.log(response[i].userId);
-                    document.getElementById('contentList').value = response[i].content;
+                    tag += '<input class="form-check-input me-2" class="contentListChk" type="checkbox" value="" aria-label="..." checked />\n' +
+                    '<span class="contentList" name="contentList" style="color: #1b1e21" value="'+response[i].content+'"></span>';
                 }
-                document.getElementById('contentList').value = xhr.response.content.value;
+                inSpan.innerHTML = tag;
+                // document.getElementById('contentList').value = xhr.response.content.value;
+
             }
         }
     };
