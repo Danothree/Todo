cb84a91c7b9941668e734f51e61de697a8ed5344
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2022-07-29 13:15:23 +0900

    feat: apiController Test 코드 추가

--- todo/todo/src/test/java/com/danoth/todo/controller/ApiTodoControllerTest.java
+++ todo/todo/src/test/java/com/danoth/todo/controller/ApiTodoControllerTest.java
@@ -4,32 +4,24 @@ import com.danoth.todo.dto.TodoDto;
 import com.danoth.todo.entity.Todo;
 import com.danoth.todo.factory.todo.TodoFactory;
 import com.danoth.todo.service.TodoService;
+import com.google.gson.Gson;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.web.reactive.server.WebTestClient;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(MockitoExtension.class)
 class ApiTodoControllerTest {
 
@@ -39,15 +31,61 @@ class ApiTodoControllerTest {
     @InjectMocks
     ApiTodoController controller;
 
+    private MockMvc mockMvc;
+    private String basicUrl = "/api/todos";
+
+    @BeforeEach
+    public void init(){
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
     @Test
     @DisplayName("POST: Todo 생성 테스트")
-    void createTest(){
-        //given
+    void createTodoTest() throws Exception {
+        //given, when, then
+        mockMvc.perform(
+                post(basicUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new Gson().toJson(createTodoDto()))
+        ).andExpect(status().isCreated());
+    }
 
-        //when
+    @Test
+    @DisplayName("DELETE : Todo 제거 테스트")
+    void deleteTodoTest() throws Exception{
+        // given, when, then
+        mockMvc.perform(
+                delete(basicUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new Gson().toJson(createTodoDto()))
+        ).andExpect(status().isOk());
+    }
 
-        //then
+    @Test
+    @DisplayName("PUT : Todo 수정 테스트")
+    void updateTodoTest() throws Exception{
+        // given, when, then
+        mockMvc.perform(
+                put(basicUrl)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new Gson().toJson(createTodoDto()))
+        ).andExpect(status().isOk());
+    }
 
+    @Test
+    @DisplayName("Success 변경 테스트")
+    void changeDoneTest() throws Exception{
+        // given, when, then
+        mockMvc.perform(put(basicUrl + "/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("Delete all completed lists 테스트")
+    void clearCompletedTest() throws Exception {
+        // given, when, then
+        mockMvc.perform(delete(basicUrl + "/completed"))
+                .andExpect(status().isOk());
     }
 
     private TodoDto createTodoDto(){
---
+++ todo/todo/src/test/java/com/danoth/todo/controller/TodoControllerTest.java
@@ -0,0 +1,33 @@
+package com.danoth.todo.controller;
+
+import com.danoth.todo.service.TodoService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class TodoControllerTest {
+
+    @InjectMocks
+    TodoController todoController;
+
+    @Mock
+    TodoService todoService;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void init(){
+        mockMvc = MockMvcBuilders.standaloneSetup(todoController).build();
+    }
+
+
+
+}
